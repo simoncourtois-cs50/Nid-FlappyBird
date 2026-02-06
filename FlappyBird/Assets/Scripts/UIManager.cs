afb551d4242408e78b5f68987f87395ae4e434d1
SimonC <>
2026-02-06 08:26:37 +0100

    ajout

--- FlappyBird/Assets/Scripts/UIManager.cs
+++ FlappyBird/Assets/Scripts/UIManager.cs
@@ -1,14 +1,12 @@
 
-using System;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
    public Button yourButton;
 
-   public GameManager m_gameManager;
+  
    private void Start()
    {
       Button btn = yourButton.GetComponent<Button>();
